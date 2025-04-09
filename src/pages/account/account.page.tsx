eda040bd167757dde3f5b2bd2ea2a8cfa6464795
Pedro Ollero <pedroollero1998@gmail.com>
2025-04-09 18:12:04 +0200

    Refactor account page imports; streamline error handling and validation model

--- src/pages/account/account.page.tsx
+++ src/pages/account/account.page.tsx
@@ -1,11 +1,12 @@
 import { AppLayout } from "@/layouts";
 import React from "react";
-import { Account, AccountFormErrors, createEmptyAccountFormErrors, emptyAccount } from "./account.model";
+import { Account, emptyAccount } from "./account.model";
 import { saveAccount } from "./api/account.api";
 import classes from "./account.page.module.css"
 import { useNavigate } from "react-router-dom";
 import { appRoutes } from "@/core/router";
 import { validateForm } from "./validation/account.validation";
+import { AccountFormErrors, createEmptyAccountFormErrors } from "./validation/account.validation.model";
 
 export const AccountPage: React.FC = () => {
   const [newAccount, setNewAccount] = React.useState<Account>(emptyAccount()
