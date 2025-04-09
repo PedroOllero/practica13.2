8b5f79511ccf47782c624f2b7bd07a736d37b298
Pedro Ollero <pedroollero1998@gmail.com>
2025-04-09 11:24:56 +0200

    Add link to create new account and enhance account form layout

--- src/pages/account/account.page.tsx
+++ src/pages/account/account.page.tsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export const AccountPage: React.FC = () => {
-  return <div>Create/Edit Account</div>;
+  return (
+    <div>
+      <div>
+        <p>Cuenta Bancaria</p>
+        <form>
+          <div>
+            <label>Tipo de cuenta</label>
+            <select>
+              <option>Ahorro</option>
+              <option>Ingreso</option>
+              <option>Seleccionar un tipo de cuenta</option>
+            </select>
+          </div>
+          <div>
+            <label>Alias</label>
+            <input></input>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
